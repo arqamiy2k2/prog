f79777c2b28142302b3f7d61155840e4ddcabd5d
arqamiy2k2 <84661806+arqamiy2k2@users.noreply.github.com>
2022-06-13 08:22:19 +0500

    Create index.c

---
+++ index.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {    
+int a;
+int b;
+	clrscr();
+	printf("First Number:");
+	scanf("%d",& a );
+	printf("Second Number:");
+	scanf("%d", &b);
+	int c=a+b; 
+	printf("Answer is : %d" ,c);
+	getch();
+}
